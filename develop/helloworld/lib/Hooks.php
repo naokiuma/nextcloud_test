a1900fb33ef069c57bf2e29c0e4dae22dbecc2d5
nao mac <sagalumaata@gmail.com>
2024-10-22 21:54:00 +0900

    test1

---
+++ develop/helloworld/lib/Hooks.php
@@ -0,0 +1,38 @@
+<?php
+// lib/Hooks.php
+
+namespace OCA\helloworld\lib;
+
+use OCP\Files\Node;
+use OCP\Files;
+use OCP\Util;
+
+class Hooks {
+
+    // フックの登録
+    public static function register() {　
+        // ファイルがアップロードされた後のフック
+        \OCP\Util::connectHook('OC_Filesystem', 'post_create', 'OCA\helloworld\lib\Hooks', 'onFileUpload');
+        // ファイルが削除された後のフック
+        \OCP\Util::connectHook('OC_Filesystem', 'post_delete', 'OCA\helloworld\lib\Hooks', 'onFileDelete');
+    }
+
+    // ファイルアップロードの処理
+    public static function onFileUpload(array $params) {
+        $path = $params['path'];
+
+		// JavaScriptでログを出力するためのコードを出力
+		echo "<script>logUpload('$path');</script>";
+		Util::writeLog('helloworld', "ファイルアップ！: $path", Util::INFO);
+
+        // // ログにメッセージを出力
+        // Util::writeLog('helloworld', "ファイルアップ！: $path", Util::INFO);
+    }
+
+    // ファイル削除の処理
+    public static function onFileDelete(array $params) {
+        $path = $params['path'];
+        // ログにメッセージを出力
+        Util::writeLog('helloworld', "ファイル削除！: $path", Util::INFO);
+    }
+}
